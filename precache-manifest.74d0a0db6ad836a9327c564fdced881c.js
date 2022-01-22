bdda668bd425b05c593fbc8087531d035237590b
SteveJamerson <37200183+SteveJamerson@users.noreply.github.com>
2022-01-21 21:56:27 -0300

    Updates

---
+++ precache-manifest.74d0a0db6ad836a9327c564fdced881c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76d6a96e7a99197a4e17bba6f03fc554",
+    "url": "/newtab-modulo-2/index.html"
+  },
+  {
+    "revision": "f9acda55b15089a9a46d",
+    "url": "/newtab-modulo-2/static/css/main.528a3875.chunk.css"
+  },
+  {
+    "revision": "87f58a76f2e04b76efe6",
+    "url": "/newtab-modulo-2/static/js/2.c7fc1d28.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/newtab-modulo-2/static/js/2.c7fc1d28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9acda55b15089a9a46d",
+    "url": "/newtab-modulo-2/static/js/main.fd588dc3.chunk.js"
+  },
+  {
+    "revision": "6b58e7af3b5db5cb05ca",
+    "url": "/newtab-modulo-2/static/js/runtime-main.f57fd52d.js"
+  }
+]);
